c4262eb02a0ef5408dbc83c89795c3b44e106c74
ilabartenev <ilia.bartenev@novardis.com>
2022-08-18 13:54:56 +0300

    Feature: motivation

--- index.ts
+++ index.ts
@@ -1,10 +1,30 @@
-const sequence = new Promise((res) => {
-  let count = 1;
-  setInterval(() => {
-    count += 1;
-    res(count);
-  });
-});
+// const sequence = new Promise((res) => {
+//   let count = 1;
+//   setInterval(() => {
+//     count += 1;
+//     res(count);
+//   });
+// });
+//
+// sequence.then((v) => console.log(v));
+// sequence.then((v) => console.log(v));
 
-sequence.then((v) => console.log(v));
-sequence.then((v) => console.log(v));
+// const sequence = function* iteratorFn() {
+//   let count = 1;
+//   while (true) {
+//     yield count++;
+//   }
+// }();
+//
+// console.log(sequence.next().value);
+// console.log(sequence.next().value);
+// console.log(sequence.next().value);
+// console.log(sequence.next().value);
+
+
+import { interval } from 'rxjs';
+
+interval(1000)
+    .subscribe((v) => {
+        console.log(v);
+    })
